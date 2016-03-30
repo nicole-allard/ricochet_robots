33087d977cb7166de506178e4d35485d3b26243e
nicole-allard <nicole.allard@gmail.com>
2016-03-29 20:36:18 -0700

    get babel actually transpiling from es6. get rid of duplicate css import

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
     loaders: [{
       test: /\.js?$/,
       exclude: /node_modules/,
-      loader: 'babel'
+      loader: 'babel-loader'
     }, {
         test: /\.css$/,
         loader: 'style!css'
